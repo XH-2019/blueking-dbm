9277828477f91cd1694888614e6559155401fc86
huangwentai-wendel <huangwentai96@163.com>
2025-07-10 15:29:46 +0800

    feat(backend): KafKa topic均衡 #11586

--- dbm-ui/backend/db_services/bigdata/kafka/views.py
+++ dbm-ui/backend/db_services/bigdata/kafka/views.py
@@ -19,6 +19,7 @@ from backend.db_services.bigdata.kafka.query import KafkaListRetrieveResource
 from backend.db_services.bigdata.resources import yasg_slz
 from backend.db_services.bigdata.resources.views import BigdataResourceViewSet
 from backend.db_services.dbbase.resources import serializers
+from backend.iam_app.dataclass.actions import ActionEnum
 
 
 @method_decorator(
@@ -75,3 +76,4 @@ class KafkaClusterViewSetBigdata(BigdataResourceViewSet):
     query_class = KafkaListRetrieveResource
     query_serializer_class = serializers.ListResourceSLZ
     db_type = DBType.Kafka
+    list_perm_actions = [ActionEnum.KAFKA_REBALANCE]
