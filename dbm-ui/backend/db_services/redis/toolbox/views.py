60d363722112f7114ac4a78d61e8c5e249fd2a64
huangwentai-wendel <huangwentai96@163.com>
2025-08-22 15:38:15 +0800

    feat(backend): redis版本升级 #12312

--- dbm-ui/backend/db_services/redis/toolbox/views.py
+++ dbm-ui/backend/db_services/redis/toolbox/views.py
@@ -89,14 +89,14 @@ class ToolboxViewSet(BaseClusterViewSet):
     def get_cluster_versions(self, request, bk_biz_id, **kwargs):
         data = self.params_validate(self.get_serializer_class())
         cluster_ids, node_type = data["cluster_ids"], data["node_type"]
-        data_list = []
+        data_map = {}
         for cluster_id in cluster_ids:
             if data["type"] == RedisVersionQueryType.ONLINE.value:
                 version_info = ToolboxHandler.get_online_cluster_versions(cluster_id, node_type)
             else:
                 version_info = ToolboxHandler.get_update_cluster_versions(cluster_id, node_type)
-            data_list.append({cluster_ids: version_info})
-        return Response(data_list)
+            data_map[cluster_id] = version_info
+        return Response(data_map)
 
     @common_swagger_auto_schema(
         operation_summary=_("查询集群可更新大版本"),
